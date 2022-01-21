56e7eaa8254a0ef3cb6db1930f72f02600edc11d
Brian Mann <hawaiilovermann@gmail.com>
2022-01-21 13:28:10 -0700

    add .gitignore file

--- my_derived_object.py
+++ my_derived_object.py
@@ -1,8 +1,8 @@
 import my_object
 
 class MyDerivedObject(my_object.MyObject):
-     def __init__(self):
-          super().__init__()
+    def __init__(self):
+        super().__init__()
 
-     def __str__(self):
-	 return self.greeting
+    def __str__(self):
+        return self.greeting
--- my_object.py
+++ my_object.py
@@ -1,3 +1,3 @@
 class MyObject:
-	def __init__(self):
-	    self.greeting = "Hello World!"
+    def __init__(self):
+        self.greeting = "Hello World!"
